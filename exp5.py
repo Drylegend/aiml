3b5191b654425f54183a6539e5df26fa10b7c145
Drylegend <130828463+Drylegend@users.noreply.github.com>
2025-05-16 00:41:54 +0530

    Update exp5.py

--- exp5.py
+++ exp5.py
@@ -1,43 +1,34 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
 
 # Load the MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Reshape input data (flatten 28x28 images into 784-dimensional vectors)
-X_train = X_train.reshape(-1, 28 * 28).astype('float32') / 255
-X_test = X_test.reshape(-1, 28 * 28).astype('float32') / 255
+# Reshape the input data to be a 1-dimensional array
+X_train = X_train.reshape(X_train.shape[0], -1)
+X_test = X_test.reshape(X_test.shape[0], -1)
 
-# One-hot encode the labels
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+# Normalize the input data
+X_train = X_train.astype('float32') / 255
+X_test = X_test.astype('float32') / 255
 
-# Build the neural network model
-model = Sequential([
-    Dense(50, input_shape=(784,), activation='relu'),
-    Dense(10, activation='softmax')
-])
+# One-hot encode the output data
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
+# Define the neural network model
+model = Sequential()
+model.add(Dense(50, input_dim=784, activation='relu'))
+model.add(Dense(10, activation='softmax'))
 # Compile the model
-model.compile(
-    loss='categorical_crossentropy',
-    optimizer='adam',
-    metrics=['accuracy']
-)
-
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Train the model
-model.fit(
-    X_train, y_train,
-    validation_data=(X_test, y_test),
-    epochs=10,
-    batch_size=200
-)
-
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
 # Evaluate the model
-loss, accuracy = model.evaluate(X_test, y_test)
-print("Test loss:", loss)
-print("Test accuracy:", accuracy)
+scores = model.evaluate(X_test, y_test)
+print("Test loss:", scores[0])
+print("Test accuracy:", scores[1])
